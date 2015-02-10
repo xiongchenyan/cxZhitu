0fafb3fb74c8955300503993164943fc521c994a
xiongchenyan <xiongchenyan@gmail.com>
2015-02-09 22:59:21 -0500

    init training data pre process

---
+++ TrainingDataPreProcess/__init__.py
@@ -0,0 +1,10 @@
+'''
+I am the package of processing training data.
+General goal:
+    given a list of training data, do:
+        1, segmentation (call LTP)
+        2, bow feature selection
+        3, word -> id mapping
+        4, generate SVM type training data
+
+'''
